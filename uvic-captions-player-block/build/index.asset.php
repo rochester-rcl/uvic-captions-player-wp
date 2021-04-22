6f4ef2fea3f48b15e33bf29b46003dcb82dea75e
jromphf <jromphf@library.rochester.edu>
2021-04-22 11:30:47 -0400

    add playerEmbed to body of block

--- uvic-captions-player-block/build/index.asset.php
+++ uvic-captions-player-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => '5c5763e85eb00aadc48b2e66e45da4bb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => '89cbbe411fb625885453b3a9779438cb');
